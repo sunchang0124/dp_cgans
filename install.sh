e0179a9b9de83bbdbc1949d666c5ea2fd07fc8a0
cudillal <cudilla.leopold@hotmail.com>
2022-07-19 14:26:28 +0000

    Modify install script to setup Poetry instead of venv

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-python -m venv venv
-source venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-deactivate
-python -m ipykernel install --user --name venv
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+export PATH="$HOME/.poetry/bin:$PATH"
+poetry env use $(which python)
+poetry install
