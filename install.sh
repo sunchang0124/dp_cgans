017f557f6a00316ab84f67c2ec819c4b0f15e4b3
cudillal <leopold.cudilla@maastrichtuniversity.nl>
2022-07-20 15:05:32 +0000

    Add OWL2Vec-star module install

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poet
 export PATH="$HOME/.poetry/bin:$PATH"
 poetry env use $(which python)
 poetry install
+poetry run python OWL2Vec-Star/setup.py install
