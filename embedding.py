b357a5ed0ac56c2db7c3215358b4dc3cdc762463
cudillal <leopold.cudilla@maastrichtuniversity.nl>
2022-07-25 07:29:17 +0000

    Add clean embedding script to the notebook

--- embedding.py
+++ embedding.py
@@ -4,20 +4,23 @@ from owl2vec_star import owl2vec_star
 
 nltk.download('punkt')
 
-#Parameters:
-# ontology_file
-# config_file
-# uri_doc
-# lit_doc
-# mix_doc
-gensim_model = owl2vec_star.extract_owl2vec_model(None, "./embedding.cfg", True, True, True)
-
 output_folder = '../persistent/data/ontology/embeddings/hpObo_hoom_ordo'
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-#Gensim format
+"""
+Parameters:
+    ontology_file
+    config_file
+    uri_doc
+    lit_doc
+    mix_doc
+    -> modify the cfg file for more params (cache dir, epochs, etc.)
+"""
+gensim_model = owl2vec_star.extract_owl2vec_model(None, "./embedding.cfg", True, True, True)
+
+# Gensim format
 gensim_model.save(os.path.join(output_folder, 'ontology.embeddings'))
 
-#Txt format
+# Text format (not required)
 gensim_model.wv.save_word2vec_format(os.path.join(output_folder, "ontology.embeddings.txt"), binary=False)
